acdcec8b47bb5cfcf545faface954e7ff8f0ed5f
Valery Gurov <va1ery@ya.ru>
2020-10-10 12:41:07 +0300

    web app project added

---
+++ Web_library_db/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Web_library_db.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
